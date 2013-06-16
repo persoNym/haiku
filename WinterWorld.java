001b747100e9736a174d2ea8c68ef3c4b79d8c75
nezu <nezu@nezu.(none)>
2013-06-16 12:28:27 -0400

    first

---
+++ WinterWorld.java
@@ -0,0 +1,28 @@
+/** WinterWork.java:  a first Java program. 
+    @author Alice F
+    @revised by Victoria C
+    @last edited 01/11/13 
+ */ 
+public class WinterWorld { 
+    
+    // Each program must have a main function like this. 
+    public static void main(String[] args) { 
+        System.out.println( "\n   Roses are red that"); 
+        System.out.println( "    much is true but "
+               +"violets\n    They are never blue\n ");
+        System.out.println( "\n    So ends my haiku...achoo");
+        System.out.println( "\n    Victoria C.");
+        
+        /**
+        Roses are red that
+    	much is true but violets
+    	They are never blue
+ 
+
+   		So ends my haiku...achoo
+
+    	Victoria C.
+
+         */
+    }  
+}
